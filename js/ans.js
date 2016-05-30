c90475b304668cd79fc509cfe84bc7e99a47bae9
Ling <CatM@Summer.local>
2016-05-30 21:15:11 +0800

    修改历史记录显示，分页

--- js/ans.js
+++ js/ans.js
@@ -19,6 +19,7 @@
         }else{
           $content.slideDown(500);
         }
+        showpages();
       })
     })
 
@@ -55,7 +56,7 @@
                 $('#usercom').html(data.content);
                 $('#userinput').html($('#test').val());
                 saveStorage('usercom');
-                loadStorage('showmsg');
+                showpages();
 
               } else {
                 alert(data.content);
@@ -103,13 +104,55 @@
       var result = "";
       for( var j = 0; j<arr.length; j++){
         // 显示样式,问题-答案展开
-        result += arr[j].value+"<hr>";
+        result += "<p>"+arr[j].value+"</p>";
 
       }
       var target = document.getElementById(id);
       target.innerHTML = result;
 
     }
+
+    // 分页
+  function showpages(){
+  var show_per_page = 5; 
+  var number_of_items = localStorage.length;
+  var number_of_pages = Math.ceil(number_of_items/show_per_page);
+  $('#current_page').val(0);
+  $('#show_per_page').val(show_per_page);
+  var navigation_html = '<a class="previous_link" href="javascript:previous();">Prev</a>';
+  var current_link = 0;
+  while(number_of_pages > current_link){
+    navigation_html += '<a class="page_link" href="javascript:go_to_page(' + current_link +')" longdesc="' + current_link +'">'+ (current_link + 1) +'</a>';
+    current_link++;
+  }
+  navigation_html += '<a class="next_link" href="javascript:next();">Next</a>';
+  $('#page_navigation').html(navigation_html);
+  $('#page_navigation .page_link:first').addClass('active_page');
+  $('#showmsg').children().css('display', 'none');
+  $('#showmsg').children().slice(0, show_per_page).css('display', 'block'); 
+}
+
+function previous(){
+  new_page = parseInt($('#current_page').val()) - 1;
+  if($('.active_page').prev('.page_link').length==true){
+    go_to_page(new_page);
+  }
+}
+function next(){
+  new_page = parseInt($('#current_page').val()) + 1;
+  //if there is an item after the current active link run the function
+  if($('.active_page').next('.page_link').length==true){
+    go_to_page(new_page);
+  }
+}
+function go_to_page(page_num){
+  var show_per_page = parseInt($('#show_per_page').val());
+  start_from = page_num * show_per_page;
+  end_on = start_from + show_per_page;
+  $('#showmsg').children().css('display', 'none').slice(start_from, end_on).css('display', 'block');
+  $('.page_link[longdesc=' + page_num +']').addClass('active_page').siblings('.active_page').removeClass('active_page');
+  $('#current_page').val(page_num);
+}
 
     // 清除历史记录
     /*$(function(){
